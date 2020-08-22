6b1d640d4753ab50c764e7f4307b9aaeb8e26803
Panister Fatheroy <panisterfatheroy@gmail.com>
2020-08-22 17:09:13 +0100

    60,000

--- index-test.js
+++ index-test.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 6000
+const express = require('express');
+const app = express();
+const port = 60000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
